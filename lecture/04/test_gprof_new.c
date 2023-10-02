468672e22310b04a85c9a29239cd168c4347ac82
G Carl Evans <gcevans@gmail.com>
2023-10-02 19:21:33 +0000

    lecture 4 code

---
+++ lecture/04/test_gprof_new.c
@@ -0,0 +1,15 @@
+//
+// from https://www.thegeekstuff.com/2012/08/gprof-tutorial/
+//
+//test_gprof_new.c
+#include<stdio.h>
+
+void new_func1(void)
+{
+    printf("\n Inside new_func1()\n");
+    int i = 0;
+
+    for(;i<0xffffffee;i++);
+
+    return;
+}
